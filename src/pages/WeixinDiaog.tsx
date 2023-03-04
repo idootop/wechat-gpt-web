ada1bd9f1be2ff4b639c1da6a6011e5e9feaf6a7
WJG <wjg12341234@163.com>
2023-03-04 23:11:25 +0800

    fix: 修复微信关注弹窗默认弹出的问题

--- src/pages/WeixinDiaog.tsx
+++ src/pages/WeixinDiaog.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import { Center } from '@/components/Flex';
 
 export function showWeixinDialog() {
@@ -8,16 +10,21 @@ export function hideWeixinDialog() {
   document.getElementById('weixin')!.style.visibility = 'hidden';
 }
 
-export const WeixinDialog = () => (
-  <Center
-    id="weixin"
-    onClick={hideWeixinDialog}
-    width="100vw"
-    height="100vh"
-    position="fixed"
-    zIndex={100}
-    background="rgba(0, 0, 0, 0.4)"
-  >
-    <img src="weixin.jpg" width="300px" style={{ borderRadius: '16px' }} />
-  </Center>
-);
+export const WeixinDialog = () => {
+  useEffect(() => {
+    hideWeixinDialog();
+  }, []);
+  return (
+    <Center
+      id="weixin"
+      onClick={hideWeixinDialog}
+      width="100vw"
+      height="100vh"
+      position="fixed"
+      zIndex={100}
+      background="rgba(0, 0, 0, 0.4)"
+    >
+      <img src="weixin.jpg" width="300px" style={{ borderRadius: '16px' }} />
+    </Center>
+  );
+};
